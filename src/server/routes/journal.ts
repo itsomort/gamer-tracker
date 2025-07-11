7ff7af520fc0fcfe19651a94d97177c80492829b
omar-wsl2 <omarsal890@gmail.com>
2025-07-11 15:37:22 -0400

    new schema and sentiment analysis framework

--- src/server/routes/journal.ts
+++ src/server/routes/journal.ts
@@ -15,7 +15,7 @@ HTTP 500 = error
 
 GET /api/journal
 req: {"userid": "hashed_code"}
-res: {"status": 0 or 1, "entries": ["entry1", "entry2"]}
+res: {"status": 0 or 1, "entries": [["entry1", sentiment1], ["entry2", sentiment2], ...]}
 "status": 0 = success, "status": 1 = error
 HTTP 200 = success
 HTTP 500 = error
@@ -23,16 +23,6 @@ HTTP 500 = error
  */
 
 router.use(json());
-// curl -X POST -d '{"hello": "hi"}' -H "Content-Type:application/json" --url http://localhost:4200/what
-router.post('/what', async (req, res) => {
-    let db = await connectToDb();
-    //console.log(process.env.ATLAS_URI)
-    let collection = await db.collection("tests");
-    let query = {_id: "la"};
-    let result = await collection.findOne(query);
-    console.log(result);
-    res.send("Whatever\n").status(200);
-});
 
 router.post('/', async(req, res) => {
     // write journal entry to document with _id of hashed_code
@@ -53,24 +43,44 @@ router.post('/', async(req, res) => {
     let query = {_id: userid};
     let result = await collection.findOne(query);
 
+    // TODO: sentiment analysis here
+    let sentiment = 0;
+
     if(result === null) {
         console.log(`User ${userid} not found`);
         // insert new document with userid and journal entry
-        await collection.insertOne({
-            "_id": userid,
-            "entries": [journal_entry]
-        });
-        res.send({"status": 0, "sentiment": 1});
+        try {
+            await collection.insertOne({
+                "_id": userid,
+                "entries": [[journal_entry, sentiment]]
+            });
+        }
+        catch {
+            res.send({"status": 1, "sentiment": 0}).status(500);
+        }
     }
     else {
         console.log(`User ${userid} found`);
         // update document with new journal entry
-        await collection.updateOne(
-            {_id: userid},
-            {$push: {entries: journal_entry}}
-        );
-        res.send({"status": 0, "sentiment": 1})
+        try {
+            await collection.updateOne(
+                {_id: userid},
+                {$push: {entries: [journal_entry, sentiment]}}
+            );
+        }
+        catch {
+            res.send({"status": 1, "sentiment": 0}).status(500);
+        }
+
     }
+
+    res.send({"status": 0, "sentiment": sentiment}).status(200);
 });
+
+router.get('/', async(req, res) => {
+    
+});
+
+
 
 export default router;
